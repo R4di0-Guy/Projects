ddbb9547a8013597c442545a05b66130d2e7f898
R4di0-Guy <139907499+R4di0-Guy@users.noreply.github.com>
2026-07-21 19:09:17 +0200

    Add files via upload

---
+++ desktop_apps/ConsoleApp_HW_19.04/ConsoleApp_HW_19.04/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Prog
+{
+    public static void Main()
+    {
+        //int[] numbers = { 1, 2, 2, 3, 1, 2 };
+        int[] numbers = {1, 2, 2, 3, 1, 2, 1, 2, 2, 3, 1, 2 , 1, 2, 2, 3, 1, 2 , 1, 2, 2, 3, 1, 2};
+        int[] used_nums = new int[3];
+        int[] nums_count=new int[3];
+        int num_counter = 0;
+        foreach (int num in numbers)
+        {
+            if (!used_nums.Contains(num))
+            {
+                used_nums[num_counter] = num;
+                nums_count[num_counter] = 1;
+                num_counter++;
+            }
+            else {
+                for (int i=0;i<num_counter;i++)
+                {
+                    if (used_nums[i]==num)
+                    {
+                        nums_count[i] += 1;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 3;i++){
+            Console.WriteLine($"{used_nums[i]}-> {nums_count[i]} razy");
+        }
+    }
+}
+
